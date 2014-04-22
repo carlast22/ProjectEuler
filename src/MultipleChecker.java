95a62938fb988362831b64ea5c0d9debfa98f541
Carla Suárez <csuarez@Carlas-MacBook-Pro.local>
2014-04-22 16:01:08 -0500

    [Exercise #1] Multiples of 3 and 5

---
+++ src/MultipleChecker.java
@@ -0,0 +1,5 @@
+public class MultipleChecker {
+    public boolean checkIfNumberIsMultipleOfThreeOrFive(int number) {
+        return false;
+    }
+}
---
+++ test/Multiples3and5Test.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+
+public class Multiples3and5Test {
+
+    @Test
+    public void testShouldReturnAllTheMultiplesOfThreeAndFiveBelowOneThousand() {
+
+    }
+}
